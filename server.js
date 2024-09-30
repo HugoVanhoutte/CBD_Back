38cda738a61151eb64ca5f25fecb570d7aa1fc3e
HugoVanhoutte <hugo.vanhoutte.pro@gmail.com>
2024-09-30 09:03:54 +0200

    init 2

---
+++ server.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.json())
+const cors = require('cors')
+app.use(cors())
